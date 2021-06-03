2b4d63e3cc6e9448a4233b4ec6fd798ac8e78fd6
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-03 19:19:26 +0530

    👨‍💻Fix

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """<b>👋Hy {},
 
 I'm an advanced filter bot with many capabilities!
 There is no practical limits for my filtering capacity :)
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @Mo_Tech_YT</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : MT Unlimited Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> <b>@Mo_Tech_YT</b>    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
